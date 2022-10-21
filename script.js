adbfec892a91f93ffc7df39cd072394c4e64f793
Ngan Do <dongocthanhngan2604@gmail.com>
2022-10-21 22:35:02 +0700

    change heading1 content again

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var count =0
 button1.addEventListener('click', function(){
     count+=1
     if(count % 2 === 0){
-        heading1.innerHTML = 'mon ga ga'
+        heading1.innerHTML = 'mon'
     }else{
         heading1.innerHTML = 'mon ko ga'
     }
