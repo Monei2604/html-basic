d77d7390485df4a98813fed1ddd138bb43c687c3
Ngan Do <dongocthanhngan2604@gmail.com>
2022-10-21 22:27:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var button1 = document.getElementById('button1')
+var heading1 = document.getElementById('heading1')
+
+var count =0
+button1.addEventListener('click', function(){
+    count+=1
+    if(count % 2 === 0){
+        heading1.innerHTML = 'mon ga'
+    }else{
+        heading1.innerHTML = 'mon ko ga'
+    }
+})
